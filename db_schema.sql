d603a3ce1fcbb6e1c69389a7d2a501373ab0624d
damian.piotrowski <damian.piotrowski@ubojniarytel.pl>
2026-04-28 16:00:50 +0200

    dodano schemat bazy

---
+++ db_schema.sql
@@ -0,0 +1,188 @@
+CREATE DATABASE  IF NOT EXISTS `miniecommerce` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `miniecommerce`;
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: miniecommerce
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `addresses` (
+  `UserID` int NOT NULL,
+  `Country` varchar(255) NOT NULL,
+  `PostalCode` varchar(255) NOT NULL,
+  `City` varchar(255) NOT NULL,
+  `Street` varchar(255) NOT NULL,
+  `BuildingNumber` varchar(255) NOT NULL,
+  `ApartmentNumber` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  KEY `FK_USER_ID_idx` (`UserID`),
+  CONSTRAINT `FK_USER_ID` FOREIGN KEY (`UserID`) REFERENCES `logindata` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `images` (
+  `GUID` varchar(255) NOT NULL,
+  `ItemID` int NOT NULL,
+  `IsPrimary` bit(1) NOT NULL,
+  PRIMARY KEY (`GUID`),
+  UNIQUE KEY `GUID_UNIQUE` (`GUID`),
+  KEY `FK_Item_ID_idx` (`ItemID`),
+  CONSTRAINT `FK_Item_ID` FOREIGN KEY (`ItemID`) REFERENCES `items` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `CreationTime` datetime NOT NULL,
+  `CreatorID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  KEY `CreatorID_idx` (`CreatorID`),
+  CONSTRAINT `CreatorID` FOREIGN KEY (`CreatorID`) REFERENCES `logindata` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `logindata`
+--
+
+DROP TABLE IF EXISTS `logindata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logindata` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `DisplayName` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `unique_username` (`Username`),
+  UNIQUE KEY `DisplayName_UNIQUE` (`DisplayName`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orderitems`
+--
+
+DROP TABLE IF EXISTS `orderitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderitems` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `OrderID` int NOT NULL,
+  `ItemID` int NOT NULL,
+  `Quantity` smallint NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_ORDERITEMS_ITEM_ID_idx` (`ItemID`),
+  KEY `FK_ORDERITEMS_ORDER_ID_idx` (`OrderID`),
+  CONSTRAINT `FK_ORDERITEMS_ITEM_ID` FOREIGN KEY (`ItemID`) REFERENCES `items` (`ID`),
+  CONSTRAINT `FK_ORDERITEMS_ORDER_ID` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_USER_ID_idx` (`UserID`),
+  CONSTRAINT `FK_USER_ID_ORDERS` FOREIGN KEY (`UserID`) REFERENCES `logindata` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sessions` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `DeviceID` varchar(255) NOT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `ExpiresAt` datetime NOT NULL,
+  `IsRevoked` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  KEY `UserID_idx` (`UserID`),
+  CONSTRAINT `UserID` FOREIGN KEY (`UserID`) REFERENCES `logindata` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tokens`
+--
+
+DROP TABLE IF EXISTS `tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tokens` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `RefreshTokenHash` varchar(255) NOT NULL,
+  `ReplacedByTokenID` int DEFAULT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `ExpiresAt` datetime NOT NULL,
+  `IsRevoked` bit(1) NOT NULL DEFAULT b'0',
+  `RevokedAt` datetime DEFAULT NULL,
+  `RevokedReason` varchar(255) DEFAULT NULL,
+  `SessionID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `SessionID_idx` (`SessionID`),
+  CONSTRAINT `FK_Session_ID` FOREIGN KEY (`SessionID`) REFERENCES `sessions` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-28 15:59:48
