be15b95aacf3e3e44d3217150cdeb1d588c9e208
damian.piotrowski <damian.piotrowski@ubojniarytel.pl>
2026-04-28 15:57:22 +0200

    Dodano rozwijaną listę do navbar

--- Front/Components/NavBar.jsx
+++ Front/Components/NavBar.jsx
@@ -1,20 +1,15 @@
 ﻿import { Link } from "react-router-dom";
-import { useState } from "react";
+
+import "../Styles/NavBarStyle.css";
 
 export default function NavBar() {
-    const [dropDownVisible, setDropDownVisible] = useState(false);
-
     const logInStatus =
         localStorage.getItem("logInStatus") !== null
             ? localStorage.getItem("logInStatus")
             : false;
 
-    function toggleDropDown() {
-        dropDownVisible === false
-            ? setDropDownVisible(true)
-            : setDropDownVisible(false);
-
-        console.log(dropDownVisible);
+    function handleDropdown() {
+        document.getElementById("dropdown-content").classList.toggle("show");
     }
 
     return (
@@ -22,9 +17,18 @@ export default function NavBar() {
             <Link to="/">Home</Link>
             <Link to="/login">Logowanie</Link>
             <Link to="/register">Rejestracja</Link>
-            {logInStatus && <Link to="/item/add">Dodaj artykuł</Link>}
-            <button onClick={toggleDropDown}>ꜜ</button>
-            {dropDownVisible && <label>dropdown</label>}
+            {logInStatus === true && (
+                <div className="dropdown">
+                    <button className="dropbtn" onClick={handleDropdown}>
+                        ꜜ
+                    </button>
+                    <div id="dropdown-content" className="dropdown-content">
+                        <Link to="/item/add" onClick={handleDropdown}>
+                            Dodaj ofertę
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
