0ea6ce500f5f87feecb6506df97c8cd0a7363f1a
damian.piotrowski <damian.piotrowski@ubojniarytel.pl>
2026-06-15 15:24:55 +0200

    fix: poprawiono adres api we froncie

--- API/Program.cs
+++ API/Program.cs
@@ -88,7 +88,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(builder.Configuration["Front"]!)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
